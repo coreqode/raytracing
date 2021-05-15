19b2f600280b95a2efd960971a748303dbb8300c
Chandradeep Pokhariya <chinu.deep100@gmail.com>
2021-05-15 19:24:03 +0530

    adding fuzziness

--- material.h
+++ material.h
@@ -31,19 +31,21 @@ class lambertian : public material{
 
 class metal: public material{
     public:
-        metal(const color& a){
+        metal(const color& a, double f){
             albedo = a;
+            fuzz = f < 1 ? f : 1;
         }
         virtual bool scatter(
                 const ray& r, const hit_record& rec, color& attenuation, ray& scattered
                 ) const override{
             vec reflected = reflect(unit_vector(r.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
     public:
         color albedo;
+        double fuzz;
 };
 
 #endif
